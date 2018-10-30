323a0579b338333637da027101c930f9610e99cc
David Grimm <dtg58@scarletmail.rutgers.edu>
2018-10-30 16:15:12 -0400

    Update 12years.py

---
+++ 12years.py
@@ -0,0 +1,4 @@
+name = raw_input('Enter "What is this?":')
+print 'This is a program that displays the effects of global warming by entering different degrees of warming.'
+name = raw_input('Enter a number between 1.0 and 2.0:')
+print 'Spike in flooding.'
