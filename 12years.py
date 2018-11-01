176e3b8951ef356da91af3be156eefc55e0766f1
David Grimm <dtg58@scarletmail.rutgers.edu>
2018-11-01 14:45:24 -0400

    Update 12years.py

--- 12years.py
+++ 12years.py
@@ -1,4 +1,8 @@
+#the following code requests an input from a user and assigns the input to a variable
+#the user will type what they want inputted and when they press enter it will be printed to the terminal
 name = raw_input('Enter "What is this?":')
+#assigns input to a variable so that it can be printed
 print 'This is a program that displays the effects of global warming by entering different degrees of warming.'
-name = raw_input('Enter a number between 1.0 and 2.0:')
+#prints the data stored in the variable
+name = raw_input('Enter a number between 1.5C and 2.0C:')
 print 'Spike in flooding.'
