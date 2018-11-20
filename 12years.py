d8e46209111b8315f36c5a15baa14226dec3fbea
David Grimm <dtg58@scarletmail.rutgers.edu>
2018-11-20 16:40:53 -0500

    Update 12years.py

--- 12years.py
+++ 12years.py
@@ -4,10 +4,14 @@ question = raw_input('Press "Enter" to find out what this program does.:')
 #assigns input to a variable so that it can be printed
 print 'This is a program that displays the effects of global warming by entering different degrees of warming.'
 #prints the data stored in the variable
+
+
 number = raw_input('Enter a number between 1.0C and 2.0C:')
-type(number)
 number = float(number)
 type(number)
+
+
+
 if number < 1.5:
     print "Improved public health"#this is if global warming reduces, showing that we have made progress
 elif number > 1.5 and number < 2.0: #this is if global warming becomes worse, showing we haven't made any progress
@@ -21,8 +25,39 @@ type(number)
 number = float(number) #this converts a string to a float because, if not, using raw_input for anything other than a string will throw an error
 type(number)
 if number < 1.5:
-    print "something good" #stand in for data I don't have yet
+    print "Preserves key ecosystems and species"
 elif number > 1.5 and number < 2.0:
-    print "something bad"#stand in for data I don't have yet
+    print "Water scarcity"
+else:
+    print "We've failed"
+
+def goodBad():
+    number = raw_input('Enter a number between 1.0C and 2.0C:')
+    number = float(number)
+    type(number)
+
+goodBad()
+if number < 1.5:
+    print "Enhanced global and national security"
+elif number > 1.5 and number < 2.0:
+    print "Longer drought periods"
+else:
+    print "We've failed"
+
+
+
+goodBad()
+if number < 1.5:
+    print "More jobs created."
+elif number > 1.5 and number < 2.0:
+    print "More frequent extreme rainfall over land."
+else:
+    print "We've failed"
+
+goodBad()
+if number < 1.5:
+    print "Cleaner water"
+elif number > 1.5 and number < 2.0:
+    print "Increaded chance of species losing over 50% of their climate range"
 else:
     print "We've failed"
